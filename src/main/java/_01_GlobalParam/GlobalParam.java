a12e89e54e27555fb81ee3daf8d36b97bae8ff0e
TizzyBac <TizzyBac TizzyBac@icloud.com>
2018-01-13 23:05:46 +0800

    修改工作目錄路徑為Global變數
    Signed-off-by: TizzyBac <TizzyBac TizzyBac@icloud.com>

--- src/main/java/_01_GlobalParam/GlobalParam.java
+++ src/main/java/_01_GlobalParam/GlobalParam.java
@@ -4,5 +4,5 @@ public class GlobalParam {
 	
 	public static final String HOST_IP = "127.0.0.1";
 	public static final Integer HOST_PORT = 6379;
-	
+	public static final String WORKSPACE_PROJECT_NAME = "E:\\CTBC_workspace__Oxygen\\RedisTest";
 }
